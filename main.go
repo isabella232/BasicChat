81d9296ca0a5e22f65095f949c08398ecb38c85e
Remi Coudert <remi.coudert@epfl.ch>
2019-08-24 12:42:47 +0200

    some cleanup

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
 )
 var server *ServerInfo
 
-type Peer string
-
 type ServerInfo struct {
 	Addr *net.UDPAddr
 	MsgBuffer []peerMessage
@@ -31,13 +29,6 @@ type setupMessage struct {
 	Peers []string
 }
 
-type downloadMessage struct {
-	MetaHash string `json:"metaHash"`
-	Contact string `json:"contact"`
-	FileName string `json:"fileName"`
-	GossipID int `json:"id"`
-}
-
 type peerMessage struct {
 	Origin string
 	Content string
@@ -50,24 +41,6 @@ type clientMessage struct {
 
 type ChatMessage struct {
 	Text string
-}
-
-type addPeerMessage struct {
-	Peer string `json:"peer"`
-	GossipID int `json:"id"`
-}
-
-type createGossiperMessage struct {
-	ID int
-	Addr string
-}
-
-type idMessage struct {
-	GossipID int `json:"id"`
-}
-
-type gossiperAddrMessage struct {
-	GossipAddr string `json:"gossiperAddress"`
 }
 
 func getMessages(w http.ResponseWriter, r *http.Request) {
@@ -143,101 +116,38 @@ func newMsg(w http.ResponseWriter, r *http.Request) {
 
 func waitForMessages() {
 
-	var connection, err = net.ListenUDP("udp4", &net.UDPAddr{server.Addr.IP, 5001, server.Addr.Zone})
+	var connection, err= net.ListenUDP("udp4", &net.UDPAddr{server.Addr.IP, 5001, server.Addr.Zone})
 	CheckError(err)
 
 	defer connection.Close()
 
-	var buffer = make([]byte, 2048)
+	var buffer= make([]byte, 2048)
 
 	for {
 
-		var sz, from, errRcv = connection.ReadFromUDP(buffer)
+		var sz, from, errRcv= connection.ReadFromUDP(buffer)
 		CheckError(errRcv)
 
 		if errRcv == nil {
 
-			var pckt = &ChatMessage{}
+			var pckt= &ChatMessage{}
 
-			var errDecode = protobuf.Decode(buffer[:sz], pckt)
+			var errDecode= protobuf.Decode(buffer[:sz], pckt)
 			CheckError(errDecode)
 
 			if pckt.Text != "" {
-				fmt.Println("RCVD MDG : \""+pckt.Text+"\" FROM :"+from.IP.String())
+				fmt.Println("RCVD MDG : \"" + pckt.Text + "\" FROM :" + from.IP.String())
 				server.MsgBuffer = append(server.MsgBuffer,
 					peerMessage{from.IP.String(), pckt.Text})
 			}
-
 
 		}
 
 	}
 
 }
-/*
-func createGossiper(w http.ResponseWriter, r *http.Request) {
-
-	var body, err = ioutil.ReadAll(r.Body)
-	CheckError(err)
-
-	body = bytes.TrimPrefix(body, []byte("\xef\xbb\xbf"))
-
-	var msg = gossiperAddrMessage{}
-	err = json.Unmarshal(body, &msg)
-	CheckError(err)
-
-	var id = len(gossipers)
-
-	var gossiper = gossipInfo{}
-
-	uiPort := strconv.Itoa(8080 + id + 1)
-	gossipAddr := msg.GossipAddr
-	gossipPort := strconv.Itoa(5000 + id)
-	udpAddrGossiper := gossipAddr+":"+gossipPort
-
-	_, err = net.ResolveUDPAddr("udp4", udpAddrGossiper)
-
-	var data []byte
-
-	if err != nil {
-
-		data, err = json.Marshal(createGossiperMessage{-1, ""}) // TODO concurrency
-		CheckError(err)
-
-	} else {
-
-		name := "GossiperGUI"+strconv.Itoa(id)
-
-		g, channel := gossip.NewGossiper(uiPort, udpAddrGossiper, name,
-			"", 10, false) // TODO rtimer
-
-		gossiper.Contacts = append(gossiper.Contacts, name)
-
-		gossiper.Channel = channel
-		gossiper.G = g
-
-		go waitForMessages(&gossiper, channel)
-
-		udpAddr, err := net.ResolveUDPAddr("udp4", "127.0.0.1:"+uiPort)
-		CheckError(err)
-
-		gossiper.Addr = udpAddr
-
-		gossipers = append(gossipers, &gossiper)
-
-		data, err = json.Marshal(createGossiperMessage{id, udpAddrGossiper}) // TODO concurrency
-		CheckError(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write(data)
-
-}*/
 
 func main() {
-
-	//gossipers = make([]*gossipInfo, 0, 100)
 
 	server = &ServerInfo{}
 
